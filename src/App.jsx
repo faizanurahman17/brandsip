74425e23aa21ceaf2eb58c329a62060d067aeb86
Faizanur Rahman <faizanurahman1711@gmail.com>
2026-01-20 18:37:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+const App = () => {
+  const brandName = "Brand Sip"
+  document.title = brandName;
+  return (
+    <div>
+      <p>{brandName}</p>
+    </div>
+  )
+}
+
+export default App
